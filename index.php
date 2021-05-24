d6f6e842210b07d9a24b15c20a405ef25f7760ca
hwalang <smdong97@gmail.com>
2021-05-24 17:24:44 +0900

    메인화면:  main 디자인 완성

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
     <meta charset="utf-8">
     <title>SAMDIDAS</title>
     <link rel="stylesheet" type="text/css" href="./css/indexStyle.css">
+    <link rel="stylesheet" type="text/css" href="./css/mainStyle.css">
 </head>
 <body>
     <header>
@@ -12,8 +13,4 @@
     <section>
         <?php include "main.php"; ?>
     </section>
-    
-    <footer>
-        <?php include "footer.php"; ?>
-    </footer>
 </body>
---
+++ main.php
@@ -0,0 +1,59 @@
+<body>
+    <div id="main_image">
+        <img src="./image/main_image.png">
+    </div>
+
+    <div id="main_container">
+        <div id="hot_item_container">
+            <h4>인기 상품</h4>
+            <ul>
+                <li class="hot_item">
+                    <div class="image_wrapper">
+                        <div class="image">
+                            <div class="image_center">
+                                <img src="./image/test_image.png">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="hot_item_name">상품이름</div>
+                    <div class="hot_item_price">가격</div>
+                </li>
+                <li class="hot_item">
+                    <div class="image_wrapper">
+                        <div class="image">
+                            <div class="image_center">
+                                <img src="./image/test_image.png">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="hot_item_name">상품이름</div>
+                    <div class="hot_item_price">가격</div>
+                </li>
+                <li class="hot_item">
+                    <div class="image_wrapper">
+                        <div class="image">
+                            <div class="image_center">
+                                <img src="./image/test_image.png">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="hot_item_name">상품이름</div>
+                    <div class="hot_item_price">가격</div>
+                </li>
+                <li class="hot_item">
+                    <div class="image_wrapper">
+                        <div class="image">
+                            <div class="image_center">
+                                <img src="./image/test_image.png">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="hot_item_name">상품이름</div>
+                    <div class="hot_item_price">가격</div>
+                </li>
+            </ul>
+        </div>
+
+
+    </div>
+</body>
