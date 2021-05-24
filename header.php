ee9b7e43a42b8a592dcf0f6f7c33f0f9751930d8
hwalang <smdong97@gmail.com>
2021-05-24 11:49:03 +0900

    메인화면: header 디자인 완성

---
+++ header.php
@@ -0,0 +1,25 @@
+<body>
+    <div id="header">
+        <div>
+            <h1 >
+                <a href="index.php" class="logo">SAMDIDAS</a>
+            </h1>
+        </div>
+    </div>
+
+    <div id="menu_bar">
+        <ul>
+            <li><a href="running_shoes.php">러닝화</a></li>
+            <li><a href="casual_shoes.php">캐주얼</a></li>
+            <li><a href="formal_shoes.php">포멀</a></li>
+        </ul>
+    </div>
+
+    <div id="small_menu">
+        <ul>
+            <li><a href="sign_up.php">회원 가입</a></li>
+            <li> | </li>
+            <li><a href="login.php">로그인</a></li>
+        </ul>
+    </div>
+</body>
---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="utf-8">
+    <title>SAMDIDAS</title>
+    <link rel="stylesheet" type="text/css" href="./css/indexStyle.css">
+</head>
+<body>
+    <header>
+        <?php include "header.php"; ?>
+    </header>
+
+    <section>
+        <?php include "main.php"; ?>
+    </section>
+    
+    <footer>
+        <?php include "footer.php"; ?>
+    </footer>
+</body>
