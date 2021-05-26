959fc8765888c82487806f34cc99fcce414de579
hwalang <smdong97@gmail.com>
2021-05-26 20:44:09 +0900

    메인화면: 회원가입/로그인 파일 연결

--- header.php
+++ header.php
@@ -10,18 +10,18 @@
     <div class="menu_bar_center">
         <div id="menu_bar">
             <ul>
-                <li><a href="running_shoes.php">러닝화</a></li>
+                <li><a href="running_shoes.php">운동화</a></li>
                 <li><a href="casual_shoes.php">캐주얼</a></li>
-                <li><a href="formal_shoes.php">포멀</a></li>
+                <li><a href="sports_shoes.php">스포츠</a></li>
             </ul>
         </div>
     </div>
 
     <div id="small_menu">
         <ul>
-            <li><a href="sign_up.php">회원 가입</a></li>
+            <li><a href="register.html">회원 가입</a></li>
             <li> | </li>
-            <li><a href="login.php">로그인</a></li>
+            <li><a href="login.html">로그인</a></li>
         </ul>
     </div>
 </body>
