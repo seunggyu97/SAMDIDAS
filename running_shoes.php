296ad95046386980e782f408fc4751ce8b29c912
hwalang <smdong97@gmail.com>
2021-05-27 22:55:38 +0900

    Escape

---
+++ running_shoes.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="utf-8">
+    <title>SAMDIDAS</title>
+    <link rel="stylesheet" type="text/css" href="./css/indexStyle.css">
+</head>
+<body>
+<header>
+    <?php include "header.php"; ?>
+</header>
+<!-- 이미지 | 이름 | 가격 작성해주세요 -->
+
+
+</body>
---
+++ sports_shoes.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="utf-8">
+    <title>SAMDIDAS</title>
+    <link rel="stylesheet" type="text/css" href="./css/indexStyle.css">
+</head>
+<body>
+<header>
+    <?php include "header.php"; ?>
+</header>
+
+
+</body>
